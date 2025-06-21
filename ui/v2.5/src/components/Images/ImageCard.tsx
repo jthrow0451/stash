5b20ae09334953649201725c0d936290604d0bfe
jthrow0451 <92414459+jthrow0451@users.noreply.github.com>
2025-06-20 23:42:59 -0700

    Update ImageCard.tsx

--- ui/v2.5/src/components/Images/ImageCard.tsx
+++ ui/v2.5/src/components/Images/ImageCard.tsx
@@ -205,6 +205,8 @@ export const ImageCard: React.FC<IImageCardProps> = (
             <ImagePreview
               loop={video}
               autoPlay={video}
+              muted={video}
+              playsInline={video}
               className="image-card-preview-image"
               alt={props.image.title ?? ""}
               src={source}
